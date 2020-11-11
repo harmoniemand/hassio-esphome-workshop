0c34cfa20604317dda440919ec4c64f4523e042e
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-11-11 02:20:07 +0100

    enabled LEDs, implemented Intervall

--- esphome/bme680/CE_BSEC.h
+++ esphome/bme680/CE_BSEC.h
@@ -5,6 +5,10 @@ class CE_BSEC : public PollingComponent, public Sensor
 {
 public:
   // constructor
+
+  int Intervall = 500;
+  int lastRun = 0;
+
   Bsec iaqSensor;
 
   Sensor *RawTemperatureSensor = new Sensor();
@@ -19,14 +23,16 @@ public:
   Sensor *Co2EquivalentSensor = new Sensor();
   Sensor *BreathVocEquivalentSensor = new Sensor();
 
+  String output;
 
   // Sensor *TSensor = new Sensor();
   // Sensor *PSensor = new Sensor();
   // Sensor *HSensor = new Sensor();
   // Sensor *AQSensor = new Sensor();
+  int errorState = 0;
   Sensor *ErrorSensor = new Sensor();
 
-  CE_BSEC() : PollingComponent(10000) {}
+  CE_BSEC() : PollingComponent(500) {}
 
   void setup() override
   {
@@ -57,35 +63,72 @@ public:
 
   void update() override
   {
+    // Serial.println(lastRun);
+    // Serial.println(Intervall);
+    // Serial.println(millis());
+
+    float rawTemperature;
+    float pressure;
+    float rawHumidity;
+    float gasResistance;
+    float iaq;
+    float iaqAccuracy;
+    float temperature;
+    float humidity;
+    float staticIaq;
+    float co2Equivalent;
+    float breathVocEquivalent;
 
     if (iaqSensor.run())
     { // If new data is available
 
-       RawTemperatureSensor->publish_state(iaqSensor.rawTemperature);
-       PressureSensor->publish_state(iaqSensor.pressure);
-       RawHumiditySensor->publish_state(iaqSensor.rawHumidity);
-       GasResistanceSensor->publish_state(iaqSensor.gasResistance);
-       IaqSensor->publish_state(iaqSensor.iaq);
-       IaqAccuracySensor->publish_state(iaqSensor.iaqAccuracy);
-       TemperatureSensor->publish_state(iaqSensor.temperature);
-       HumiditySensor->publish_state(iaqSensor.humidity);
-       StaticIaqSensor->publish_state(iaqSensor.staticIaq);
-       Co2EquivalentSensor->publish_state(iaqSensor.co2Equivalent);
-       BreathVocEquivalentSensor->publish_state(iaqSensor.breathVocEquivalent);
+      float rawTemperature = iaqSensor.rawTemperature;
+      float pressure = iaqSensor.pressure;
+      float rawHumidity = iaqSensor.rawHumidity;
+      float gasResistance = iaqSensor.gasResistance;
+      float iaq = iaqSensor.iaq;
+      float iaqAccuracy = iaqSensor.iaqAccuracy;
+      float temperature = iaqSensor.temperature;
+      float humidity = iaqSensor.humidity;
+      float staticIaq = iaqSensor.staticIaq;
+      float co2Equivalent = iaqSensor.co2Equivalent;
+      float breathVocEquivalent = iaqSensor.breathVocEquivalent;
 
+      if (lastRun < millis() - Intervall)
+      {
+        lastRun = millis();
+        unsigned long time_trigger = millis();
+
+        RawTemperatureSensor->publish_state(rawTemperature);
+        PressureSensor->publish_state(pressure);
+        RawHumiditySensor->publish_state(rawHumidity);
+        GasResistanceSensor->publish_state(gasResistance);
+        IaqSensor->publish_state(iaq);
+        IaqAccuracySensor->publish_state(iaqAccuracy);
+        TemperatureSensor->publish_state(temperature);
+        HumiditySensor->publish_state(humidity);
+        StaticIaqSensor->publish_state(staticIaq);
+        Co2EquivalentSensor->publish_state(co2Equivalent);
+        BreathVocEquivalentSensor->publish_state(breathVocEquivalent);
+
+        output = String(time_trigger);
+        output += ", " + String(rawTemperature);
+        output += ", " + String(pressure);
+        output += ", " + String(rawHumidity);
+        output += ", " + String(gasResistance);
+        output += ", " + String(iaq);
+        output += ", " + String(iaqAccuracy);
+        output += ", " + String(temperature);
+        output += ", " + String(humidity);
+        output += ", " + String(staticIaq);
+        output += ", " + String(co2Equivalent);
+        output += ", " + String(breathVocEquivalent);
+        Serial.println(output);
+      }
     }
     else
     {
       checkIaqSensorStatus();
-    }
-
-    if (iaqSensor.status != BSEC_OK)
-    {
-      ErrorSensor->publish_state(1);
-    }
-    else
-    {
-      ErrorSensor->publish_state(0);
     }
   }
 
@@ -97,12 +140,14 @@ public:
       {
         // output = "BSEC error code : " + String(iaqSensor.status);
         // Serial.println(output);
+        errorState = 1;
         ErrorSensor->publish_state(1);
       }
       else
       {
         // output = "BSEC warning code : " + String(iaqSensor.status);
         // Serial.println(output);
+        errorState = 2;
         ErrorSensor->publish_state(2);
       }
     }
@@ -112,18 +157,24 @@ public:
       {
         // output = "BME680 error code : " + String(iaqSensor.bme680Status);
         // Serial.println(output);
+        errorState = 1;
         ErrorSensor->publish_state(1);
       }
       else
       {
         // output = "BME680 warning code : " + String(iaqSensor.bme680Status);
         // Serial.println(output);
+        errorState = 2;
         ErrorSensor->publish_state(2);
       }
     }
     else
     {
-      ErrorSensor->publish_state(0);
+      if (errorState != 0)
+      {
+        ErrorSensor->publish_state(0);
+        errorState = 0;
+      }
     }
   }
 };
